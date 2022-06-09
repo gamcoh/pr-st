190d48723b43fa4af316712ef5962bc5cf8e3cc9
Gamliel Cohen <gamliel.cohen-ext@pernod-ricard.com>
2022-06-09 14:30:41 +0200

    Initial commit

---
+++ pr_st/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""PR-ST is a CLI package that helps creates streamlit templates"""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
